f1bc6dd988df9b3ddac8955237ba684b5fef0750
Tom Alvarez <tmlvrz@gmail.com>
2024-10-10 17:12:01 +0200

    03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    for (let i = string.length - 1; i>=0; i--) {
+        result += string[i];
+    }
+    return result
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+/*
+
+declare variable for reversed string
+for loop through the string starting from the end
+log each character from the reverse loop into the reversed string variable
+return reversed string
+
+*/
