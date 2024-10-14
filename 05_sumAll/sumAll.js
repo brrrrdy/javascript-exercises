023887479b0435661ab7ccce26c138d59153a8ac
Tom Alvarez <tmlvrz@gmail.com>
2024-10-14 15:46:50 +0200

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return 'ERROR';
+    }
+    
+    let numHigh = Math.max(int1, int2);
+    let numLow = Math.min(int1, int2);
+    let sum = 0;
 
+    for (let i = numLow; i <= numHigh; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
