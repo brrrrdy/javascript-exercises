a0ddf550d4ef0d252f21266c2e6de16f524815d2
Tom Alvarez <tmlvrz@gmail.com>
2024-10-14 16:32:38 +0200

    07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheight) {
+  const frTemp = ((farenheight - 32)*5/9);
+  return Math.round(frTemp*10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const csTemp = (celcius*(9/5) +32);
+  return Math.round(csTemp*10) / 10;
 };
 
 // Do not edit below this line
