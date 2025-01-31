94bc7bcc1a63f7d24d3e0d5d08000e895bc7d3e0
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-01-31 15:51:42 +0100

    palindromes js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (p) {
+    const cleanedPalindrome = p.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let isPalindrome = '';
+    for (let i = cleanedPalindrome.length - 1; i >= 0; i--) {
+        isPalindrome += cleanedPalindrome[i];
+    }
+    return cleanedPalindrome === isPalindrome;
 
+    // let cleanedPalindrome (remmove non alphanumeric and special characters from given string, convert all to lowercase)
+    // let isPalindrome
+
+
+
+    // let isPalindrome = '';
+    // for (let i = p.length - 1; i >= 0; i--) {
+    //     isPalindrome += p[i]; 
+    //     if (isPalindrome === p) {
+    //         return true;
+    //     }
+    // }
+
+    // loop backwards over string and log to variable
+    // is this same as string
 };
 
 // Do not edit below this line
